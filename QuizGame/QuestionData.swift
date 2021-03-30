626f58c285decc895e1219ab052947c9e6b0a4dd
a.m.class <allofmehis4u@gmail.com>
2021-03-30 08:41:09 -0500

    updated

--- QuizGame/QuestionData.swift
+++ QuizGame/QuestionData.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answer: [Answer]
+}
+
+enum ResponseType {
+    case question1, question2, question3
+}
+
+struct Answer{
+    var text: String
+    var type: CharacterType
+}
+
+enum CharacterType: String {
+    case cat = "Cat", dog = "Dog", rabbit = "Rabbit", bird = "Bird"
+
+var definition: String {
+    switch self {
+    case .cat:
+        return "You may sometimes be rude and overbearing but inside you are kind and affectionate, you spend most of your days alone but if you so choose you could spend your day with someone else"
+    case .dog:
+        return "You are playful, curious, and sociabal. You can be aggressive sometimes if someone doesn't treat you or your frinds right"
+    case .rabbit:
+        return "Intelligent, affectionate, sociable, and sometimes a little shy. You can also be bratty, willful, destructive and even vengeful. It take a special type of person to be able to coexist happily with such a complex, demanding little soul."
+    case .bird:
+        return "You are free nothing can describe you, you describe yourself the way you want to represent yourself to others. Between your groups you may be the funny one, the party player, the shy one, or anything you choose who you want to be with others and yourself."
+        }
+    }
+}
--- QuizGame/QuestionViewController.swift
+++ QuizGame/QuestionViewController.swift
@@ -8,22 +8,113 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var questionOneStackView: UIStackView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    
+    @IBOutlet weak var questionTwoStackView: UIStackView!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    
+    
+    @IBOutlet weak var questionThreeStackView: UIStackView!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    var question: [Question] = [
+        Question(text: "How do you spend your days?", type: .question1, answer: [
+        Answer(text: "I spend the day having alone time", type: .cat),
+        Answer(text: "I spend the day hanging out with friends", type: .rabbit),
+        Answer(text: "I spend the day exploring the outdoors", type: .dog),
+        Answer(text: "I spend the days in other ways", type: .bird)
+        ]),
+        Question(text: "Would you descibe yourself has", type: .question2, answer: [
+            Answer(text: "Intelligent", type: .rabbit),
+            Answer(text: "Playful", type: .dog),
+            Answer(text: "Self Right", type: .cat),
+            Answer(text: "Other", type: .bird)
+        ]),
+        Question(text: "What was your favorite game when you were a child?", type: .question3, answer: [
+            Answer(text: "Playing sports", type: .dog),
+            Answer(text: "Hide and seek", type: .cat),
+            Answer(text: "Puzzle/board games", type: .bird),
+            Answer(text: "Playing with friends", type: .rabbit)
+        ])
+    ]
+    
+    var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        
+        questionOneStackView.isHidden = true
+        questionTwoStackView.isHidden = true
+        questionThreeStackView.isHidden = true
+        
+        navigationItem.title = ("Question #\(questionIndex + 1)")
+        
+        let currentQuestion = question[questionIndex]
+        let currentAnswer = currentQuestion.answer
+        let totalProgress = Float(questionIndex) / Float(question.count)
+        
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        switch currentQuestion.type {
+        case.question1:
+            updateQuestionOneStackView(using: currentAnswer)
+        case.question2:
+            questionTwoStackView.isHidden = false
+            updateQuestionTwoStackView(using: currentAnswer)
+        case.question3:
+            questionThreeStackView.isHidden = false
+            updateQuestionThreeStackView(using: currentAnswer)
+            
+        }
     }
-    */
-
+    
+    func updateQuestionOneStackView(using answers: [Answer]) {
+        questionOneStackView.isHidden = false
+        button1.setTitle(answers[0].text, for: .normal)
+        button2.setTitle(answers[1].text, for: .normal)
+        button3.setTitle(answers[2].text, for: .normal)
+        button4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateQuestionTwoStackView(using answers: [Answer]) {
+        questionTwoStackView.isHidden = false
+        button5.setTitle(answers[0].text, for: .normal)
+        button6.setTitle(answers[1].text, for: .normal)
+        button7.setTitle(answers[2].text, for: .normal)
+        button8.setTitle(answers[3].text, for: .normal)
+        
+    }
+    
+    func updateQuestionThreeStackView(using answers: [Answer]) {
+        questionThreeStackView.isHidden = false
+        button9.setTitle(answers[0].text, for: .normal)
+        button10.setTitle(answers[1].text, for: .normal)
+        button11.setTitle(answers[2].text, for: .normal)
+        button12.setTitle(answers[3].text, for: .normal)
+    }
+    
 }
